12f0b6588cbe2196d1ba6dccb21c4e2281902b4a
Lanil Marasinghe <lanil.marasinghe@syscolabs.com>
2019-05-17 09:40:51 +0530

    loading views based on cookies and cookie generation

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const SECRET = 'supersecret';
 
 app.listen(3000);
+app.use(cookieParser());
 
 const authorization = function (req, res, next) {
     const token = req.headers['x-authorization'];
@@ -17,11 +19,33 @@ const authorization = function (req, res, next) {
     });
 };
 
+app.get('/', function (req, res) {
+    const cookie = req.cookies['x-authorization-cookie'];
+    if (cookie) {
+        jwt.verify(cookie, SECRET, function (err, decoded) {
+            res.sendFile('views/home.html', {root: __dirname})
+        });
+    } else {
+        res.clearCookie('x-authorization-cookie');
+        res.sendFile('views/login.html', {root: __dirname})
+    }
+});
 
-app.post('/login', function (req, res) {
-    const token = jwt.sign({foo: 'bar'}, SECRET);
-    console.log(token);
-    res.send({token});
+
+app.get('/login', function (req, res) {
+    const username = req.query.username;
+    const password = req.query.password;
+    if (username === 'admin' && password === 'admin') {
+        const token = jwt.sign({foo: 'bar'}, SECRET);
+        const currentDate = Date.now();
+        res.cookie('x-authorization-cookie', token, {maxAge: 300000, httpOnly: true});
+        console.log(token);
+        res.sendFile('views/home.html', {root: __dirname})
+    } else {
+        const msg = {auth: false, message: 'Invalid username or password.'};
+        res.sendFile('views/login.html', {root: __dirname})
+    }
+
 });
 
 app.get('/api/students', authorization, function (req, res) {
